5fc07ef47ae168692780aa3c7c156c8a32703e79
Xin Li <happyxin331@icloud.com>
2025-11-25 18:51:26 -0800

    complete wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,9 +1,51 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const letterPool = {
+    'A': 9,
+    'B': 2,
+    'C': 2,
+    'D': 4,
+    'E': 12,
+    'F': 2,
+    'G': 3,
+    'H': 2,
+    'I': 9,
+    'J': 1,
+    'K': 1,
+    'L': 4,
+    'M': 2,
+    'N': 6,
+    'O': 8,
+    'P': 2,
+    'Q': 1,
+    'R': 6,
+    'S': 4,
+    'T': 6,
+    'U': 4,
+    'V': 2,
+    'W': 2,
+    'X': 1,
+    'Y': 2,
+    'Z': 1
+  };
+  const poolList = [];
+  for (const [letter, count] of Object.entries(letterPool)) {
+    for (let i = 0; i < count; i++) {
+      poolList.push(letter);
+    }
+  };
+  const drawn = [];
+  for (let i = 0; i < 10; i++) {
+    const randomIndex = Math.floor(Math.random() * poolList.length);
+    drawn.push(poolList[randomIndex]);
+    poolList.splice(randomIndex,1)
+  };
+  return drawn;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
+  
 };
 
 export const scoreWord = (word) => {
